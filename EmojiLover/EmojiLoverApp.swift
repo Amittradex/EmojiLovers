a8cef5f3792a1fc774b0f62c83c89843c66a8432
AmittradeX <amitpatel28092001@gmail.com>
2023-10-16 01:55:53 +0530

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Amit Patel on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
